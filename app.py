edeba879f99e82971cae532c0aa41cdbcfec1367
Noorts <50550612+Noorts@users.noreply.github.com>
2021-09-25 23:40:48 +0200

    Add after request CORS hook

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)  # creating the Flask class object
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 @app.route("/", methods=['GET'])
 def hello():
     data = {"key": "home page value"}
