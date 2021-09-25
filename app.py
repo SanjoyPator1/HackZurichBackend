77a447adbb05f85b6de8beca7126956053a67bce
Noorts <50550612+Noorts@users.noreply.github.com>
2021-09-25 23:25:21 +0200

    Add specific CORS origins

--- app.py
+++ app.py
@@ -15,11 +15,9 @@ from flask_cors import CORS, cross_origin
 
 # FLASK code
 app = Flask(__name__)  # creating the Flask class object
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/", methods=['GET'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def hello():
     data = {"key": "home page value"}
     data1 = json.dumps(data)
@@ -30,7 +28,6 @@ def hello():
 
 
 @app.route("/rssi", methods=['GET', 'POST'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 # Function to convert a CSV to JSON
 # Takes the file paths as arguments
 def make_json():
@@ -71,7 +68,6 @@ geoJson = json.load(f)
 
 @app.route("/geoFeature", methods=['GET', 'POST'])
 # Function to convert a CSV to JSON
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_geo():
     print("geo")
     # print(geoJson)
@@ -130,7 +126,6 @@ grJson = json.load(g)
 
 
 @app.route("/graphRssi", methods=['GET', 'POST'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 # Function to convert a CSV to JSON
 def get_graph():
     print("graph")
